25f034066ee4200daa29a5a2466aff0ca8f6681a
Dancows <mrsdancow@gmail.com>
2023-09-13 21:10:20 +0800

    first commit

---
+++ app/Views/layouts/masterweb.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Gunung Harta | Transport Solutions</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link href="https://fonts.googleapis.com/css?family=Lato&amp;display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700,900&amp;display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="<?= base_url('assets/css/fontawesome-all.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/font-mytravel.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/animate.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/hs.megamenu.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/jquery.mCustomScrollbar.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/flatpickr.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap-select.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/slick.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/css/theme.css') ?>">
+</head>
+
+<body>
+    <header id="header" class="u-header u-header--abs-top u-header--white-nav-links-xl u-header--bg-transparent u-header--show-hide border-bottom border-xl-bottom-0 border-color-white" data-header-fix-moment="500" data-header-fix-effect="slide">
+
+    </header>
+
+    <main id="content">
+        <?= $this->renderSection('content') ?>
+    </main>
+
+    <footer class="footer mt-4">
+        <div class="space-bottom-2 space-top-1">
+            <div class="container">
+                <div class="row justify-content-xl-between">
+                    <div class="col-12 col-lg-6 mb-6 mb-md-10 mb-xl-0">
+                        <div class ="d-md-flex d-lg-block">
+                            <div class="mb-5 mr-md-7 mr-lg-0">
+                                <a href="<?= base_url('') ?>">
+                                    <div class="d-flex align-items-center">
+                                        <div class="mr-4">
+                                            <img src="<?= base_url('assets/images/logo/logo.png') ?>" alt="" width="100">
+                                        </div>
+                                        <div>
+                                            <h4 class="h6 font-weight-bold mb-2 font-size-17">PT. Gunung Harta</h4>
+                                            <span class="mb-2 h6 font-weight-normal text-gray-1">Transport, Paket Kilat Dan Tour</span>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="ml-md-6 ml-lg-0">
+                                <div class="mb-4">
+                                    <address class="pr-4">
+                                        <span class="mb-2 h6 font-weight-normal text-gray-1 text-justify">
+                                            Gunung Harta adalah salah satu operator bus antarkota dan pariwisata yang berbasis di Tabanan, Bali (GHT) dan Malang, Jawa Timur (GHTS).
+                                        </span>
+                                    </address>
+                                </div>
+                                <div class="mb-4">
+                                    <h4 class="h6 font-weight-bold mb-2 font-size-17">Kantor Pusat Tabanan</h4>
+                                    <address class="pr-4">
+                                        <span class="mb-2 h6 font-weight-normal text-gray-1">
+                                            Jl. Ngurah Rai No. 70 Tabanan-Bali
+                                            <br>Telp. (0361) 811397, 814866, 811479, 810735, 811614, 814999, 085 637 06 100
+                                            <br>Fax. (0361) 813955.
+                                            <br>HP. +08123863434
+                                        </span>
+                                    </address>
+                                </div>
+                                <ul class="list-inline mb-0">
+                                    <li class="list-inline-item mr-2">
+                                        <a target="_blank" class="btn btn-icon btn-social btn-bg-transparent" href="https://www.facebook.com/GunungHartaOfficial/">
+                                            <span class="fab fa-facebook-f btn-icon__inner"></span>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item mr-2">
+                                        <a target="_blank" class="btn btn-icon btn-social btn-bg-transparent" href="https://www.youtube.com/channel/UCOXoCdFo8I_qgFN_wtycdTA">
+                                            <span class="fab fa-youtube  btn-icon__inner"></span>
+                                        </a>
+                                    </li>
+                                    <li class="list-inline-item mr-2">
+                                        <a target="_blank" class="btn btn-icon btn-social btn-bg-transparent" href="https://www.instagram.com/gunungharta/">
+                                            <span class="fab fa-instagram btn-icon__inner"></span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-12 col-md-4 col-lg-4 col-xl-1dot8 mb-6 mb-md-10 mb-xl-0">
+                        <h4 class="h6 font-weight-bold mb-2 mb-xl-4">LINK</h4>
+                        <ul class="list-group list-group-flush list-group-borderless mb-0">
+                            <li><a class="text-decoration-on-hover list-group-item list-group-item-action" href="#">Profil Armada</a></li>
+                            <li><a class="text-decoration-on-hover list-group-item list-group-item-action" href="#">Tarif</a></li>
+                            <li><a class="list-group-item list-group-item-action text-decoration-on-hover" href="#">Layanan Kami</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-12 col-lg-6 col-xl-3dot1 mb-6 mb-md-10 mb-xl-0">
+                        <div class ="d-md-flex d-lg-block">
+                            <div class="mb-2 mr-md-7 mr-lg-0">
+                                <h4 class="h6 mb-4 font-weight-bold">BOOKING ONLINE</h4>
+                                <a href="https://wa.me/6282145444544/?text=Halo, Saya ingin memesan tiket bus." target="_blank">
+                                    <div class="d-flex align-items-center">
+                                        <div class="mr-4">
+                                            <img src="<?= base_url('assets/images/media/WhatsApp.svg.png') ?>" alt="" width="50" height="50">
+                                        </div>
+                                        <div>
+                                            <div class="mb-2 h6 font-weight-normal text-gray-1">Operator online tiketing</div>
+                                            <small class="d-block font-size-18 font-weight-normal text-primary">Call: <span class="text-primary font-weight-semi-bold">+6282145444544</span></small>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="ml-md-6 ml-lg-0">
+                                <div class="mb-4">
+                                    <h4 class="h6 font-weight-bold mb-2 font-size-17">Kantor Denpasar</h4>
+                                    <address class="pr-4">
+                                        <span class="mb-2 h6 font-weight-normal text-gray-1">
+                                            Jl. Diponegoro No.53 
+                                            <br>Telp. (0361) 257688
+                                            <br>HP. 085 7391 40565, 085 637 04 800
+                                        </span>
+                                    </address>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="border-top border-bottom border-color-8 space-1">
+            <div class="container">
+                <div class="row d-block d-xl-flex align-items-md-center">
+                    <div class="col-xl-4 mb-4 mb-xl-0">
+                        <a class="d-inline-flex align-items-center" href="<?= base_url('')?>" aria-label="Gunung Harta">
+                            <img src="https://gunungharta.com/web/wp-content/uploads/2021/12/Logo-Gunung-Harta.png" alt="">
+                            <!-- <span class="brand brand-dark">PT. Gunung Harta</span> -->
+                        </a>
+                    </div>
+                    <div
+                        class="bootstrap-select__custom col-xl-8 d-block d-lg-flex justify-content-xl-end align-items-center">
+                        <div class="mb-4 mb-lg-0">
+                            <ul class="d-flex list-unstyled mb-0">
+                                <li class="mr-2 ml-0">
+                                    <img class="max-width-13" src="<?= base_url('assets/images/bank/Bank_Central_Asia.svg.png') ?>" alt="Bank BCA">
+                                </li>
+                                <li class="mx-2">
+                                    <img class="max-width-15" src="<?= base_url('assets/images/bank/1280px-BANK_BRI_logo.svg.png') ?>" alt="BANK BRI">
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="space-1">
+            <div class="container">
+                <div class="d-lg-flex d-md-block justify-content-center align-items-center">
+                    <p class="text-muted mb-3 mb-lg-0 text-gray-1">© 2023 PT. Gunung Harta. All rights reserved</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <div id="jsPreloader" class="page-preloader">
+        <div class="page-preloader__content-centered">
+            <div class="spinner-grow text-primary" role="status">
+                <span class="sr-only">Mohon Tunggu...</span>
+            </div>
+        </div>
+    </div>
+
+    <script src="<?= base_url('assets/js/jquery.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery-migrate.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/popper.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+
+    <script src="<?= base_url('assets/js/hs.megamenu.js') ?>"></script>
+    <script src="<?= base_url('assets/js/jquery.validate.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/flatpickr.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/bootstrap-select.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/slick.js') ?>"></script>
+
+    <script src="<?= base_url('assets/js/hs.core.js') ?>"></script>
+    <script src="<?= base_url('assets/js/hs.header.js') ?>"></script>
+    <script src="<?= base_url('assets/js/hs.unfold.js') ?>"></script>
+    <script src="<?= base_url('assets/js/hs.validation.js') ?>"></script>
+    <script src="<?= base_url('assets/js/hs.show-animation.js') ?>"></script>
+    <script src="<?= base_url('assets/js/hs.range-datepicker.js') ?>"></script>
+    <script src="<?= base_url('assets/js/hs.selectpicker.js') ?>"></script>
+    <script src="<?= base_url('assets/js/hs.go-to.js') ?>"></script>
+    <script src="<?= base_url('assets/js/hs.slick-carousel.js') ?>"></script>
+    <script src="<?= base_url('assets/js/hs.quantity-counter.js') ?>"></script>
+
+    <script>
+        $(window).on('load', function () {
+            // initialization of HSMegaMenu component
+            $('.js-mega-menu').HSMegaMenu({
+                event: 'hover',
+                pageContainer: $('.container'),
+                breakpoint: 1199.98,
+                hideTimeOut: 0
+            });
+
+            // Page preloader
+            setTimeout(function () {
+                $('#jsPreloader').fadeOut(500)
+            }, 800);
+        });
+
+        $(document).on('ready', function () {
+            // initialization of header
+            $.HSCore.components.HSHeader.init($('#header'));
+
+            // initialization of unfold component
+            $.HSCore.components.HSUnfold.init($('[data-unfold-target]'));
+
+            // initialization of show animations
+            $.HSCore.components.HSShowAnimation.init('.js-animation-link');
+
+            // initialization of datepicker
+            $.HSCore.components.HSRangeDatepicker.init('.js-range-datepicker');
+
+            // initialization of select
+            $.HSCore.components.HSSelectPicker.init('.js-select');
+
+            // initialization of quantity counter
+            $.HSCore.components.HSQantityCounter.init('.js-quantity');
+
+            // initialization of slick carousel
+            $.HSCore.components.HSSlickCarousel.init('.js-slick-carousel');
+
+            // initialization of go to
+            $.HSCore.components.HSGoTo.init('.js-go-to');
+        });
+    </script>
+</body>
+
+</html>
